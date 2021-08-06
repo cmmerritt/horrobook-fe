86899570a1b044e5e975c4e4ff4d9d941f4c8da7
cmmerritt <christiane.merritt@gmail.com>
2021-08-06 16:47:55 -0700

    create book presentational component

---
+++ src/components/app/books/Book.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Book = ({ title, author, imageUrl, infoUrl, pubDate }) => (
+  <figure>
+    <img src={imageUrl} alt={title} height="200px" />
+    <figcaption>
+      <p>{title}</p>
+      <p>{author}</p>
+      <p>{imageUrl}</p>
+      <p>{infoUrl}</p>
+      <p>{pubDate}</p>
+    </figcaption>
+  </figure>
+);
+
+Book.propTypes = {
+  title: PropTypes.string.isRequired,
+  author: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string,
+  infoUrl: PropTypes.string,
+  pubDate: PropTypes.string
+};
+
+export default Book;
