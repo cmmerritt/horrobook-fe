b526dc4e0525eebe21c5b024be52100b0649d9fb
cmmerritt <christiane.merritt@gmail.com>
2021-08-09 11:41:49 -0700

    start to redo front end to form rather than list from api

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import BookList from '../books/BookList';
-import BookDetail from '../details/BookDetail';
+import Form from '../forms/Form';
 import FavoriteList from '../favorites/FavoriteList';
 
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={BookList} />
-        <Route exact path="/:id" component={BookDetail} />
+        <Route exact path="/" component={Form} />
         <Route exact path="/favorites" component={FavoriteList} />
       </Switch>
     </Router>
