70100dedc2d0456a8f33f9c88de52039c9b5ee24
cmmerritt <christiane.merritt@gmail.com>
2021-08-09 22:50:15 -0700

    delete unnecessary columns on favorites

--- src/components/favorites/Favorite.jsx
+++ src/components/favorites/Favorite.jsx
@@ -2,20 +2,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Favorite = ({ title, author, description, pubDate }) => (
+const Favorite = ({ title, author }) => (
   <>
     <p>{title}</p>
     <p>{author}</p>
-    <p>{description}</p>
-    <p>{pubDate}</p>
   </>
 );
 
 Favorite.propTypes = {
   title: PropTypes.string.isRequired,
   author: PropTypes.string,
-  description: PropTypes.string,
-  pubDate: PropTypes.string
 };
 
 export default Favorite;
