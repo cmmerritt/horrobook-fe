bf262f5dcd806f836ea15699f499aad3e1b1cff2
cmmerritt <christiane.merritt@gmail.com>
2021-08-11 00:33:19 -0700

    remove cors proxy after back end fix

--- src/services/horrobookApi.js
+++ src/services/horrobookApi.js
@@ -3,7 +3,7 @@ import request from 'superagent';
 
 export const addFavorite = async (favorite) => {
   const res = await request
-    .post('https://cors-anywhere.herokuapp.com/https://fierce-sands-18083.herokuapp.com/api/v1/favorites')
+    .post('https://fierce-sands-18083.herokuapp.com/api/v1/favorites')
     .send(favorite);
   console.log(res.body);
   return res.body;
@@ -11,6 +11,6 @@ export const addFavorite = async (favorite) => {
 
 export const getFavorites = async () => {
   const res = await request
-    .get('https://cors-anywhere.herokuapp.com/https://fierce-sands-18083.herokuapp.com/api/v1/favorites');
+    .get('https://fierce-sands-18083.herokuapp.com/api/v1/favorites');
   return res.body;
 };
